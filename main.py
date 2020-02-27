de759f8837a0c36b8c270e60da84f32415fda46e
Viktor M <viktor.mikalsen@gmail.com>
2020-02-27 15:46:48 +0100

    Lagt til kommentarer og rydda litt i oppg. 1

--- main.py
+++ main.py
@@ -13,22 +13,22 @@ m = 1
 #z = np.arange(L, 1.9*L, h)
 z0 = L/(2*hbar) * np.sqrt(2*m*v0)
 
-
+#Definerer funksjonene
 def funk1(z): return np.tan(z) - np.sqrt(z0**2/z**2 - 1)
 def funk2(z): return np.tan(z+pi/2) - np.sqrt(z0**2/z**2 - 1)
 def energi(z): return (2*hbar**2*z**2)/(m*L**2) -v0
 
-def plotKrys(f):
+
+#Plotter løsningene av ligning f mhp. z
+def plotKryss(f, x):
     warnings.filterwarnings('ignore')
-    z = np.arange(0, 4*pi, .01)
+    z = np.arange(0, z0, .01)
     y = f(z)
     y[:-1][np.diff(y) < 0] = np.nan
-    init_gues = [1.4, 4.4, 7.3, 10.3]
-    x = fsolve(f, init_gues)
     plt.plot(x, np.zeros(len(x)), 'x', markersize=7)
     plt.plot(z, y)
 
-    intervall = np.arange(0, 4*pi, pi)
+    intervall = np.arange(0, z0, pi)
     vertikal = [-20, 20]
     for i in intervall:
         plt.plot([i, i], vertikal, '--', color='black', linewidth='.8')
@@ -36,40 +36,53 @@ def plotKrys(f):
     plt.ylim(-20, 20)
 
 
-def plotEnerg(energi):
+#Plotter energinivåene
+def plotEnergi(energi):
     x = np.array([0., 10.])
     for e in energi:
         plt.plot(x, [e, e])
     plt.xticks([])
 
-init_gues1 = [1.4, 4.4, 7.3, 10.3]
-x1 = fsolve(funk1, init_gues1)
 
-init_gues2 = [2.4, 5.4, 8.3, 11.3]
-x2 = fsolve(funk2, init_gues2)
+#Finner løsningene av funk1 og funk2 mtp. z
+init_gues1 = np.array([1.4, 4.4, 7.3, 10.3])
+z1 = fsolve(funk1, init_gues1)
+e1 = energi(z1)
 
-e1 = energi(x1)
-e2 = energi(x2)
+init_gues2 = np.array([2.4, 5.4, 8.3, 11.3])
+z2 = fsolve(funk2, init_gues2)
+e2 = energi(z2)
 
-#* Dette er for å dele plotten inn i intervaller med bare ét nullpunkt
-x = np.arange(0, 4*pi, pi/2)
+
+# Dette er for å dele plotten inn i intervaller med bare ét nullpunkt
+x = np.arange(0, z0, pi/2)
 y = [-20, 20]
 
+
+#Plotter løsningene for z og energinivåene
 def mkfig_op1():
     plt.figure(figsize=(10, 4))
     plt.subplot(121)
-    plotKrys(funk1)
+    plotKryss(funk1, z1)
+    plt.title("Løsninger med tan(z)")
     plt.subplot(122)
-    plotEnerg(e1)
+    plotEnergi(e1)
+    plt.title("Energi")
     plt.show()
 
     plt.figure(figsize=(10, 4))
     plt.subplot(121)
-    plotKrys(funk2)
+    plotKryss(funk2, z2)
+    plt.title("Løsninger med tan(z + pi/2)")
     plt.subplot(122)
-    plotEnerg(e2)
+    plotEnergi(e2)
+    plt.title("Energi")
     plt.show()
 
+
+
+
+mkfig_op1()
 
 '''
 #Vet ikke om dette trengs
@@ -84,4 +97,3 @@ def wave_in(x, E):
     return c*sin(k*x) + d*cos(k*x)
 '''
 
-mkfig_op1()
