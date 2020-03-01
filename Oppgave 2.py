1545245854463fc4598bf0ee1b47858190c9118f
Toby Storli <events@stud.ntnu.no>
2020-03-01 23:16:34 +0100

    Endret litt på jevn, og plotter

--- Oppgave 2.py
+++ Oppgave 2.py
@@ -8,11 +8,10 @@ hbar = 1
 m = 1
 Dx = .01
 w = 1
-L = pi
 v0 = 8
+L = 2*pi
 
-v = lambda x: -v0 if (abs(x-L/2)<L/2) else 0
-
+v = lambda x: -v0 if (x<0<L) else 0
 def potensial(x): return 0.5*m*w**2*x**2
 
 #n = odde = eksisterte tilstand nr. n
@@ -34,9 +33,9 @@ def psi_jevn(E, V, L):
     x = np.arange(0, L, Dx, dtype=np.float64)
     psiH = np.zeros(len(x))
     psiH[0] = 1
-    psiH[1] = (m*Dx**2/hbar**2 *(V(x[0]) - E) + 1)*psiH[0]
+    psiH[1] = (m*Dx**2/hbar**2 * (V(x[0]) - E) +1)*psiH[0]
     for i in range(len(x)-2):
-        psiH[i+2] = 2*m*Dx**2/hbar**2*(V(x[i+1]- E))*psiH[i+1] + 2*psiH[i+1] - psiH[i] 
+        psiH[i+2] = 2*m*Dx**2/hbar**2 * (V(x[i+1]) - E)*psiH[i+1] + 2*psiH[i+1] - psiH[i]
     #    print(psiH[i+1])
     #Pga. symmetri, og for å ikke ta med 'psi0' to ganger:
     psiV = psiH[-1:0:-1]
@@ -44,21 +43,22 @@ def psi_jevn(E, V, L):
     x = np.append(-x[-1:0:-1], x)
     return x, psi
 
-'''
-for i in np.arange(-10, 10, .1):
-    x_j, psi_j = psi_jevn(i, potensial, 2*np.pi)
-    print(max(psi_j), end=": ")
-    print(i)
-'''
+x, psi = psi_jevn(.49*w*hbar, v, 2*np.pi)
+x1, psi1 = psi_jevn(.51*w*hbar, v, 2*np.pi)
+xo, psio = psi_odde(.49*w*hbar, v, 2*np.pi)
+x1o, psi1o = psi_odde(.51*w*hbar, v, 2*np.pi)
 
-x_j, psi_j = psi_jevn(-7, v, 10*np.pi)
-x_o, psi_o = psi_odde(-10, v, 10*np.pi)
-
-#plt.plot(x_j, psi_j)
-plt.plot(x_o, psi_o)
-#t = int(len(psi_j)/2)
+print(psi)
+plt.plot([x[0], x[-1]], [0,0], linestyle='--', linewidth=.8, color='black') # Lager en line y=0
+plt.plot(x, psi)
+plt.plot(x1, psi1, 'g')
 
 #plt.show()
 #plt.plot(x, potensial(x))
 plt.show()
 
+
+plt.plot(xo, psio)
+plt.plot([x[0], x[-1]], [0, 0], linestyle='--', linewidth=.8, color='black') # Lager en line y=0
+plt.plot(x1o, psi1o, 'g')
+plt.show()
